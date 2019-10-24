533721f4b49709e4c65982c2e4ee4c4e4ff531ed
pascualbeatriz <pascuals.beatriz@gmail.com>
2019-10-24 21:46:49 +0200

    Add Hero section and make timer experiments

--- assets/javascripts/site.js
+++ assets/javascripts/site.js
@@ -29,3 +29,41 @@ if (document.getElementById('count-down-days')) {
     },
     countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
 }
+
+const endDate = new Date('Nov 09, 2019 00:00:00').getTime();
+
+const countdowndate = () => {
+  let now = new Date().getTime(); 
+  let t = endDate - now; 
+
+  if(t>=0){
+    let days = Math.floor(t / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
+    let secs = Math.floor((t % (1000 * 60)) / 1000);
+
+    document.getElementById('timer-days').innerHTML = days + 
+    "<p class='date__day'>día(s)</p>";
+    
+    document.getElementById('timer-hours').innerHTML= ("0" + hours).slice(-2) +
+    "<p class='date__hours'>hora(s)</p>";
+    
+    document.getElementById('timer-mins').innerHTML= ("0" + mins).slice(-2) +
+    "<p class='date__mins'>mins</p>";
+    
+    document.getElementById('timer-segs').innerHTML= ("0" + secs).slice(-2) +
+    "<p class='date__segs'>secs</p>";
+  } else {
+    document.getElementById('timer').innerHTML = "Ha llegado el día de FabadaConf!" 
+  }
+};
+
+setInterval(countdowndate, 1000);
+
+
+
+
+
+
+          
+          
